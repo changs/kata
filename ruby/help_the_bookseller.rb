1912549b699a86cdb623358388af10055e0933c9
Bartosz Pranczke <bartosz.pranczke@gmail.com>
2021-08-27 09:17:26 +0200

    Working solution for 'Help the bookseller' kata

--- ruby/help_the_bookseller.rb
+++ ruby/help_the_bookseller.rb
@@ -1,3 +1,10 @@
+require 'pry'
 def stockList(listOfArt, listOfCat)
-
+  r = Hash.new(0)
+  listOfArt.each do |art|
+    category = art.split[0][0]
+    quantity = art.split[1].to_i
+    r[category] += quantity
+  end
+  listOfCat.map { |cat| "(#{cat} : #{r[cat]})" }.join(' - ')
 end
