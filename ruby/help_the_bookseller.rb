5cef0d076888b7bb742a0f32236fe909f4ba6a64
Bartosz Pranczke <bartosz.pranczke@gmail.com>
2021-08-27 09:21:38 +0200

    Add guard clause

--- ruby/help_the_bookseller.rb
+++ ruby/help_the_bookseller.rb
@@ -1,4 +1,5 @@
 def stockList(listOfArt, listOfCat)
+  return '' if listOfArt.empty? || listOfCat.empty?
   r = Hash.new(0)
   listOfArt.each do |art|
     art.split.tap { |cat, quantity| r[cat[0]] += quantity.to_i }
