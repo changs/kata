d9b8440847fef348eb421fd38f84608f56da2170
Bartosz Pranczke <bartosz.pranczke@gmail.com>
2021-08-28 12:37:36 +0200

    Solution for 'character with longest consecutive repetition' using regexp

--- ruby/character_with_longest_consecutive_repetition.rb
+++ ruby/character_with_longest_consecutive_repetition.rb
@@ -1,3 +1,8 @@
+require 'pry'
 def longest_repetition(chars)
+  return ['', 0] if chars.empty?
 
+  matches = chars.to_enum(:scan, /(\w)\1{0,}/).map { Regexp.last_match }
+  lengths = matches.map { |m| m[0].to_s.length }
+  [matches[lengths.index(lengths.max)][0].to_s[0], lengths.max]
 end
